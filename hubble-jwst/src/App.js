a9372561604baded40e7359eedbaa324f5d250a4
Harsha <160235068+yellowflickerbeat@users.noreply.github.com>
2025-03-27 02:00:53 +0530

    Add files via upload

---
+++ hubble-jwst/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SpaceImageComparator from "./components/SpaceImageComparator";
+
+function App() {
+  return (
+    <div>
+      <SpaceImageComparator />
+    </div>
+  );
+}
+
+export default App;
